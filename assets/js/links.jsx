7887d40da836faefb7fd5c5d2b84e26658c87aa5
Krushil <krushilnaik96@gmail.com>
2021-03-16 16:57:57 -0400

    begin React-ifying the page

---
+++ assets/js/links.jsx
@@ -0,0 +1,78 @@
+const React = require("react");
+
+// @ts-ignore
+const jsx = require("react/jsx-runtime");
+
+{/* <nav>
+	<div id="links">
+		<div id="github" data-aos="fade-right" data-aos-delay="1000">
+			<div class="hvr-underline-from-center">
+				<i class="fab fa-github-square"></i>
+				<a href="https://github.com/krushilnaik">GitHub</a>
+			</div>
+		</div>
+		<div style="width: 15px;"></div>
+		<div id="linkedin" data-aos="fade-right" data-aos-delay="800">
+			<div class="hvr-underline-from-center">
+				<i class="fab fa-linkedin"></i>
+				<a href="https://www.linkedin.com/in/krushil-naik-0a42191ba/">LinkedIn</a>
+			</div>
+		</div>
+		<div style="width: 15px;"></div>
+		<div id="email" data-aos="fade-right" data-aos-delay="600">
+			<div class="hvr-underline-from-center">
+				<i class="fas fa-envelope-square"></i>
+				<a href="mailto:krushilnaik96@gmail.com">Gmail</a>
+			</div>
+		</div>
+	</div>
+</nav> */}
+
+class NavBarLink extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div id={this.props.id} data-aos="fade-right" data-aos-delay={this.props.delay}>
+				<div className="hvr-underline-from-center">
+					<i className={this.props.icon}></i>
+					{this.props.link}
+				</div>
+			</div>
+		);
+	}
+}
+
+class NavBar extends React.Component {
+	render() {
+		const linkContent = [
+			{
+				id: "github",
+				link: <a href="https://github.com/krushilnaik">GitHub</a>,
+				icon: "fab fa-github-square"
+			},
+			{
+				id: "linkedin",
+				link: <a href="https://www.linkedin.com/in/krushil-naik-0a42191ba/">LinkedIn</a>,
+				icon: "fab fa-linkedin"
+			},
+			{
+				id: "gmail",
+				link: <a href="mailto:krushilnaik96@gmail.com">Gmail</a>,
+				icon: "fas fa-envelope-square"
+			}
+		]
+
+		return (
+			<React.Fragment>
+				{
+					linkContent.map(
+						(linkEl, i) => <NavBarLink delay={600 + (linkContent.length - i)*200} id={linkEl.id} icon={linkEl.icon}></NavBarLink>
+					)
+				}
+			</React.Fragment>
+		); 
+	}
+}

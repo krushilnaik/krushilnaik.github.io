1dc3ac07491f8b0a5b6d23fc18a12b3d863a76f4
Krushil <krushilnaik96@gmail.com>
2021-03-16 20:56:27 -0400

    parse (rather crudely) skills.json and render skills onto page

--- assets/js/skill-grid.jsx
+++ assets/js/skill-grid.jsx
@@ -1,7 +1,80 @@
+class SubSkillCard extends React.Component {
+	constructor(props) {
+		super(props);
+
+		/**
+		 * @type {string}
+		 */
+		this.skillName = props.data.skill;
+
+		/**
+		 * @type {string[]}
+		 */
+		this.repos = props.data.repos;
+	}
+
+	render() {
+		return (
+			<div className="sub-skill-card" id={this.skillName.toLowerCase().replace(" ", "-")} data-descr={this.skillName}>
+				<ul>
+					{
+						this.repos.map(
+							repo => <li><a href={repo.startsWith("http") ? repo : `https://krushilnaik.github.io/${repo}`}>{repo.replace(/-/g, " ")}</a></li>
+						)
+					}
+				</ul>
+			</div>
+		);
+	}
+}
+
 class SkillCard extends React.Component {
-	// 
+	render() {
+		return (
+			<div id={this.props.cardTitle.replace(" ", "-").toLowerCase()} data-descr={this.props.cardTitle}>
+				<div className="sub-skill-grid">
+					{this.props.subSkills.map(skill => <SubSkillCard data={skill}/>)}
+				</div>
+			</div>
+		);
+	}
 }
 
 class SkillGrid extends React.Component {
-	// 
+	constructor(props){
+		super(props);
+
+		/**
+		 * @typedef {object} SubSkill
+		 * @property {string} skill
+		 * @property {string[]} repos
+		 */
+
+		/**
+		 * @typedef {object} Skill
+		 * @property {string} skill
+		 * @property {SubSkill[]} sub_skills 
+		 */
+
+		/**
+		 * @type {Skill[]}
+		 */
+		this.skills = props.skills;
+	}
+
+	render() {
+		return (
+			<React.Fragment>
+				{this.skills.map(skill => <SkillCard cardTitle={skill.skill} subSkills={skill.sub_skills}/>)}
+			</React.Fragment>
+		);
+	}
 }
+
+fetch("assets/json/skills.json")
+	.then(response => response.json())
+	.then(
+		(data) => {
+			ReactDOM.render(<SkillGrid skills={data}/>, document.getElementById("skill-grid"));
+		}
+	);

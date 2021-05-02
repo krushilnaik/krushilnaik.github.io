ffc875d31cc31c3e4db14e10e22f7ec5b3f8b5bd
Krushil <krushilnaik96@gmail.com>
2021-05-02 12:51:21 -0400

    WIP styling improvements
    - migrate to SCSS
    - restructure project to eliminate #root element
    - add animations to headers

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
-import React from 'react';
+import anime from 'animejs';
+import React, { Fragment, useEffect, useRef } from 'react';
 
-import './App.css';
+import './scss/App.scss';
 
 function App() {
+	let wipRef = useRef(null);
+	let tipRef = useRef(null);
+
+	useEffect(() => {
+		wipRef.current = anime({
+			targets: '#wip',
+			opacity: [0, 1],
+			translateY: [-150, 0],
+			duration: 850,
+			easing: 'linear'
+		});
+
+		tipRef.current = anime({
+			targets: '.tip',
+			translateY: [-100, 0],
+			opacity: [0, 1],
+			duration: 350,
+			easing: 'linear',
+			delay: 500
+		});
+	}, []);
+
 	return (
-		<div className='App'>
-			<span>Work in Progress</span>
-		</div>
+		<Fragment>
+			<noscript>You need to enable JavaScript to view this site.</noscript>
+
+			<h1 id='wip'>Work in Progress</h1>
+			<h3 className='tip'>Check back later</h3>
+		</Fragment>
 	);
 }
 

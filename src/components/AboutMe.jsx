c8d65690d3bfea52eddaeecff5b94e8d9df8afeb
Krushil Naik <krushilnaik96@gmail.com>
2021-05-12 21:41:47 -0400

    Begin working on About me

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 import './scss/AboutMe.scss';
 
 function AboutMe() {
-	return <section id='about-me'>About me</section>;
+	return (
+		<section id='about-me'>
+			{/* I'll list all the languages I know here */}
+			{/* Basically my resume but more stylish */}
+		</section>
+	);
 }
 
 export default AboutMe;

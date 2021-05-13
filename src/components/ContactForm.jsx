d31d96ada990ba7b004fbe01102967f62e6781c9
Krushil Naik <krushilnaik96@gmail.com>
2021-05-12 21:42:15 -0400

    Adapt contact form to new page layout

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -8,42 +8,44 @@ function ContactForm() {
 	const [message, setMessage] = useState('');
 
 	return (
-		<form action='POST'>
-			<h3>Caught your attention?</h3>
+		<section id='contact-form'>
+			<form action='POST'>
+				<h3>Caught your attention?</h3>
 
-			<div id='name' className='input-wrapper'>
-				<input
-					type='text'
-					name='name'
-					// id='name'
-					placeholder='Name:'
-					defaultValue={name}
-					onChange={event => setName(event.currentTarget.value)}
-				/>
-			</div>
-			<div id='email' className='input-wrapper'>
-				<input
-					type='email'
-					name='email'
-					// id='email'
-					placeholder='Email:'
-					defaultValue={email}
-					onChange={event => setEmail(event.currentTarget.value)}
-				/>
-			</div>
-			<div id='message' className='input-wrapper'>
-				<textarea
-					name='message'
-					// id='message'
-					cols={30}
-					rows={10}
-					placeholder='Message:'
-					defaultValue={message}
-					onChange={event => setMessage(event.currentTarget.value)}
-				/>
-			</div>
-			<button type='submit'>Send carrier pigeon</button>
-		</form>
+				<div id='name' className='input-wrapper'>
+					<input
+						type='text'
+						name='name'
+						// id='name'
+						placeholder='Name:'
+						defaultValue={name}
+						onChange={event => setName(event.currentTarget.value)}
+					/>
+				</div>
+				<div id='email' className='input-wrapper'>
+					<input
+						type='email'
+						name='email'
+						// id='email'
+						placeholder='Email:'
+						defaultValue={email}
+						onChange={event => setEmail(event.currentTarget.value)}
+					/>
+				</div>
+				<div id='message' className='input-wrapper'>
+					<textarea
+						name='message'
+						// id='message'
+						cols={30}
+						rows={10}
+						placeholder='Message:'
+						defaultValue={message}
+						onChange={event => setMessage(event.currentTarget.value)}
+					/>
+				</div>
+				<button type='submit'>Send carrier pigeon</button>
+			</form>
+		</section>
 	);
 }
 

66732bfbd543060e15e803319fc44db7fd236016
Krushil Naik <krushilnaik96@gmail.com>
2021-05-13 14:15:54 -0400

    Add some information about me

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
+import { calculateAge } from '../util/js/functions';
 
 import './scss/AboutMe.scss';
 
 function AboutMe() {
 	return (
 		<section id='about-me'>
-			{/* I'll list all the languages I know here */}
-			{/* Basically my resume but more stylish */}
-			About me
+			{/* Introduce myself. Name, age, etc. */}
+			{/* Add a picture of me */}
+			{/* Hobbies/what I do in my free time */}
+			<div className='content'>
+				<aside>
+					<img src='assets/images/avatar.png' alt='crucial avatar' />
+					<div className='name'>
+						<p>Krushil Naik</p>
+						<span className='note'>
+							( pronounced <span style={{ color: 'crimson' }}>'crucial'</span> )
+						</span>
+					</div>
+					<p>{calculateAge('09/07/1996')} years old</p>
+				</aside>
+
+				<article>This is where info about me will go. Hobbies, all that good stuff.</article>
+			</div>
 		</section>
 	);
 }
--- src/util/js/functions.js
+++ src/util/js/functions.js
@@ -5,3 +5,23 @@
 export function slugify(string) {
 	return string.toLowerCase().replace(/ /g, '-');
 }
+
+/**
+ * Get number of years since a certain date
+ * @param {string} date - date in format MM/DD/YYYY
+ */
+export const calculateAge = date => {
+	const birthDate = new Date(date);
+	const todaysDate = new Date();
+
+	var years = todaysDate.getFullYear() - birthDate.getFullYear();
+
+	if (
+		todaysDate.getMonth() < birthDate.getMonth() ||
+		(todaysDate.getMonth() == birthDate.getMonth() && todaysDate.getDate() < birthDate.getDate())
+	) {
+		years--;
+	}
+
+	return years;
+};

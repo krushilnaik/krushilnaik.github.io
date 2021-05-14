e820f549dae01233d41bd4d0fb122ad67d9cf434
Krushil Naik <krushilnaik96@gmail.com>
2021-05-14 12:08:58 -0400

    Add social media links

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -1,5 +1,5 @@
 // import { faAddressBook } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 
 import './scss/Links.scss';
@@ -7,7 +7,18 @@ import './scss/Links.scss';
 function Links() {
 	return (
 		<div className='links'>
-			<FontAwesomeIcon icon={'linkedin'} color='white' />
+			{/* GitHub, LinkedIn, Twitter */}
+			<a href='https://github.com/krushilnaik' target='_blank' rel='noreferrer'>
+				<i className='fab fa-github-square'></i>
+			</a>
+
+			<a href='https://www.linkedin.com/in/krushilnaik/' target='_blank' rel='noreferrer'>
+				<i className='fab fa-linkedin'></i>
+			</a>
+
+			<a href='https://twitter.com/KrushilNaik' target='_blank' rel='noreferrer'>
+				<i className='fab fa-twitter-square'></i>
+			</a>
 		</div>
 	);
 }

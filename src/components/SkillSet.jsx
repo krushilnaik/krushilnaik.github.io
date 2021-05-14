856d1bde1a4d51a5aec8ffb29d1653c32df61820
Krushil Naik <krushilnaik96@gmail.com>
2021-05-14 11:09:15 -0400

    Add some of my skills

--- src/components/SkillSet.jsx
+++ src/components/SkillSet.jsx
@@ -7,7 +7,21 @@ function SkillSet() {
 		<section id='skill-set'>
 			{/* I'll list all the languages I know here */}
 			{/* Basically my resume but more stylish */}
-			Skills go here
+
+			{/* This is the 'languages' */}
+			<div>HTML5</div>
+			<div>CSS3</div>
+			<div>JavaScript (ES2018)</div>
+			<div>ReactJS</div>
+			<div>ExpressJS</div>
+			<div>NodeJS</div>
+			<div>AnimeJS</div>
+			<div>SCSS</div>
+
+			{/* The programs */}
+			<div>Netlify</div>
+			<div>Heroku</div>
+			<div>Git/GitHub</div>
 		</section>
 	);
 }

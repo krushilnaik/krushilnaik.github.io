6e0ccdb222620be2a80085a631b91b9c6c279381
Krushil Naik <krushilnaik96@gmail.com>
2021-05-15 18:34:23 -0400

    Get started on the skills list

--- src/components/SkillSet.jsx
+++ src/components/SkillSet.jsx
@@ -5,23 +5,40 @@ import './scss/SkillSet.scss';
 function SkillSet() {
 	return (
 		<section id='skill-set'>
-			{/* I'll list all the languages I know here */}
-			{/* Basically my resume but more stylish */}
+			<div className='content'>
+				<div className='skill-group'>
+					<h3>Languages</h3>
+					<ul>
+						<li>JavaScript</li>
+						<li>HTML5</li>
+						<li>CSS3</li>
+						<li>Python 3</li>
+						<li>C++</li>
+						<li>Java</li>
+					</ul>
+				</div>
+				<div className='skill-group'>
+					<h3>Frameworks/Technologies</h3>
+					<ul>
+						<li>ReactJS</li>
+						<li>Angular</li>
+						<li>ExpressJS</li>
+						<li>NodeJS</li>
+						<li>AnimeJS</li>
+						<li>Webpack</li>
+					</ul>
+				</div>
 
-			{/* This is the 'languages' */}
-			<div>HTML5</div>
-			<div>CSS3</div>
-			<div>JavaScript (ES2018)</div>
-			<div>ReactJS</div>
-			<div>ExpressJS</div>
-			<div>NodeJS</div>
-			<div>AnimeJS</div>
-			<div>SCSS</div>
-
-			{/* The programs */}
-			<div>Netlify</div>
-			<div>Heroku</div>
-			<div>Git/GitHub</div>
+				<div className='skill-group'>
+					<h3>Miscellaneous</h3>
+					<ul>
+						<li>SCSS</li>
+						<li>Netlify</li>
+						<li>Heroku</li>
+						<li>Gatsby</li>
+					</ul>
+				</div>
+			</div>
 		</section>
 	);
 }

252d9654fa545750bf131511b6327ea7e3c2d90e
Krushil Naik <krushilnaik96@gmail.com>
2021-05-15 22:07:44 -0400

    Implement most of the skill set animations
    - The red underline will eventually be "drawn" arcoss post-play

--- src/components/SkillSet.jsx
+++ src/components/SkillSet.jsx
@@ -1,10 +1,44 @@
-import React from 'react';
+import anime from 'animejs';
+import React, { useEffect } from 'react';
 
 import './scss/SkillSet.scss';
 import skills from '../json/skills.json';
 import { slugify } from '../util/js/functions';
 
 function SkillSet() {
+	useEffect(() => {
+		anime({
+			targets: `.skill-group h3`,
+			duration: 500,
+			translateX: [300, 0],
+			opacity: [0, 1],
+			easing: 'easeOutQuad',
+			delay: anime.stagger(200),
+			autoplay: false
+		});
+
+		anime({
+			targets: '.skill-groups h3::before',
+			duration: 300,
+			width: ['0', '100%'],
+			easing: 'linear',
+			delay: 500,
+			autoplay: false
+		});
+
+		for (const group of skills.map(skills => skills.group)) {
+			anime({
+				targets: `#${slugify(group)} ul li`,
+				duration: 500,
+				translateY: [150, 0],
+				opacity: [0, 1],
+				easing: 'easeOutQuad',
+				delay: anime.stagger(100, { start: 500 }),
+				autoplay: false
+			});
+		}
+	});
+
 	return (
 		<section id='skill-set'>
 			<div className='content'>

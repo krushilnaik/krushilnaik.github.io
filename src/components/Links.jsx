ff9eee30cd8fd1e924fc4c11e1a65e7dfd8f2d57
Krushil Naik <krushilnaik96@gmail.com>
2021-05-17 09:55:16 -0400

    Add tooltip to links "footer"

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -1,27 +1,40 @@
-import React from 'react';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { Fragment } from 'react';
 
 import './scss/Links.scss';
 
 function Links() {
 	return (
-		<div className='links'>
-			{/* GitHub, LinkedIn, Twitter, Gmail */}
-			<a id='github' href='https://github.com/krushilnaik' target='_blank' rel='noreferrer'>
-				<i className='fab fa-github-square'></i>
-			</a>
+		<Fragment>
+			<div className='popper'>
+				<span>Or use the links below</span>
+				<FontAwesomeIcon icon={faChevronDown} />
+			</div>
+			<div className='links'>
+				{/* GitHub, LinkedIn, Twitter, Gmail */}
+				<a id='github' href='https://github.com/krushilnaik' target='_blank' rel='noreferrer'>
+					<i className='fab fa-github-square'></i>
+				</a>
 
-			<a id='linkedin' href='https://www.linkedin.com/in/krushilnaik/' target='_blank' rel='noreferrer'>
-				<i className='fab fa-linkedin'></i>
-			</a>
+				<a
+					id='linkedin'
+					href='https://www.linkedin.com/in/krushilnaik/'
+					target='_blank'
+					rel='noreferrer'
+				>
+					<i className='fab fa-linkedin'></i>
+				</a>
 
-			<a id='twitter' href='https://twitter.com/KrushilNaik' target='_blank' rel='noreferrer'>
-				<i className='fab fa-twitter-square'></i>
-			</a>
+				<a id='twitter' href='https://twitter.com/KrushilNaik' target='_blank' rel='noreferrer'>
+					<i className='fab fa-twitter-square'></i>
+				</a>
 
-			<a id='email' href='mailto:krushilnaik96@gmail.com' target='_blank' rel='noreferrer'>
-				<i className='fas fa-envelope-square'></i>
-			</a>
-		</div>
+				<a id='email' href='mailto:krushilnaik96@gmail.com' target='_blank' rel='noreferrer'>
+					<i className='fas fa-envelope-square'></i>
+				</a>
+			</div>
+		</Fragment>
 	);
 }
 

227504d2281e3f15f59af3f89becffbcec80e583
Krushil Naik <krushilnaik96@gmail.com>
2021-05-26 08:02:34 -0400

    Bug fixes
    - Skill set no longer animates every time it enters the viewport

--- src/components/SkillSet.jsx
+++ src/components/SkillSet.jsx
@@ -1,47 +1,14 @@
-import anime from 'animejs';
-import React, { useContext, useEffect, useRef } from 'react';
+import React, { useContext } from 'react';
 
 import './scss/SkillSet.scss';
 import skills from '../json/skills.json';
 import { slugify } from '../utils/js/functions';
 import InView from 'react-intersection-observer';
 import { PageContext } from '../utils/js/contexts';
+import ScrollAnimation from 'react-animate-on-scroll';
 
 function SkillSet() {
 	const { setActivePage } = useContext(PageContext);
-
-	/**
-	 * @type {React.MutableRefObject<anime.AnimeInstance[]>}
-	 */
-	let animationDrivers = useRef([]);
-
-	useEffect(() => {
-		animationDrivers.current.push(
-			anime({
-				targets: `.skill-group h3`,
-				duration: 500,
-				translateX: [300, 0],
-				opacity: [0, 1],
-				easing: 'easeOutQuad',
-				delay: anime.stagger(200),
-				autoplay: false
-			})
-		);
-
-		for (const group of skills.map(skills => skills.group)) {
-			animationDrivers.current.push(
-				anime({
-					targets: `#${slugify(group)} ul li`,
-					duration: 500,
-					translateY: [150, 0],
-					opacity: [0, 1],
-					easing: 'easeOutQuad',
-					delay: anime.stagger(100, { start: 500 }),
-					autoplay: false
-				})
-			);
-		}
-	});
 
 	return (
 		<InView
@@ -50,22 +17,35 @@ function SkillSet() {
 			threshold={0.7}
 			onChange={(inView, _entry) => {
 				inView && setActivePage('What I know');
-				animationDrivers.current.forEach(animation => animation.play());
 			}}
 		>
 			<div className='content'>
-				{skills.map(skillGroup => (
+				{skills.map((skillGroup, i) => (
 					<div
 						key={slugify(skillGroup.group)}
 						id={slugify(skillGroup.group)}
 						className='skill-group'
 					>
-						<h3>{skillGroup.group}</h3>
+						<ScrollAnimation
+							animateIn='fadeInRight'
+							animateOnce={true}
+							delay={i * (500 / skills.length)}
+						>
+							<h3>{skillGroup.group}</h3>
+						</ScrollAnimation>
 						<ul>
 							{/* Plan to have each of these link both a deployed site using that technology */}
 							{/* or the GitHub repo behind the deployed site. Site viewer's choice */}
-							{skillGroup.skills.map(skill => (
-								<li key={slugify(skill)}>{skill}</li>
+							{skillGroup.skills.map((skill, j) => (
+								<li key={slugify(skill)}>
+									<ScrollAnimation
+										animateIn='fadeInUp'
+										animateOnce={true}
+										delay={j * (500 / skillGroup.skills.length) + 100}
+									>
+										{skill}
+									</ScrollAnimation>
+								</li>
 							))}
 						</ul>
 					</div>

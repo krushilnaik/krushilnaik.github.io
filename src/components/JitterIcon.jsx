f22a49eee35f5aba24071aaf7e4a51ac6ca3f5f8
Krushil Naik <krushilnaik96@gmail.com>
2021-06-22 14:45:34 -0400

    Implement icon shake when clicking "ghost" link

--- src/components/JitterIcon.jsx
+++ src/components/JitterIcon.jsx
@@ -14,20 +14,44 @@ function JitterIcon(props) {
 	const isDemo = type === 'demo';
 	const tooltip = isDemo ? 'Demo' : 'Source Code';
 
-	console.log(`Making JitterIcon linked to ${href}`);
+	const iconRef = useRef(null);
 
+	/**
+	 * @type {React.MutableRefObject<anime.AnimeInstance>}
+	 */
 	const animationRef = useRef(null);
+
+	/**
+	 * @param {React.SyntheticEvent<HTMLAnchorElement>} event
+	 */
+	const handleClick = event => {
+		if (!href) {
+			event.preventDefault();
+			animationRef.current.play();
+		}
+	};
 
 	useEffect(() => {
 		animationRef.current = anime({
-			//
+			targets: iconRef.current,
+			keyframes: [{ translateX: 0 }, { translateX: -10 }, { translateX: 10 }, { translateX: 0 }],
+			easing: 'easeOutInBounce',
+			autoplay: false,
+			duration: 250
 		});
 	});
 
 	return (
-		<a href={href} className={isDemo ? 'demo' : 'source'} rel='noreferrer' target='blank'>
+		<a
+			onClick={handleClick}
+			href={href}
+			className={isDemo ? 'demo' : 'source'}
+			rel='noreferrer'
+			target='blank'
+		>
 			<div className='icon-wrapper' data-tooltip={href ? tooltip : `No ${tooltip} available`}>
 				<i
+					ref={iconRef}
 					// id={`${href.slice(-5)}-${type}-icon`}
 					className={`fas fa-${isDemo ? 'pager' : 'file-code'}`}
 				></i>
